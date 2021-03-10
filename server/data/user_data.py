78db149d5f3c55ab2e1116854c4057d63cefa90b
Tash Alderson <aldersonnatasha@gmail.com>
2021-03-10 12:30:06 +0000

    user model schema and data

---
+++ server/data/user_data.py
@@ -0,0 +1,8 @@
+from models.user import User
+
+list_users = [
+    User(username='admin', password='admin', email='admin@admin.com'),
+    User(username='tash', password='tash', email='tash@tash.com'),
+    User(username='emily', password='emily', email='emily@emily.com'),
+    User(username='joe', password='joe', email='joe@joe.com')
+]
---
+++ server/serializers/user.py
@@ -0,0 +1,17 @@
+from app import ma
+from models.user import User
+from marshmallow import fields
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+
+    class Meta:
+        model = User
+        load_instance = True
+        exclude = ('password_hash',)
+        load_only = ('email', 'password')
+
+    password = fields.String(required=True)
+
+class SimpleUserSchema(ma.SQLAlchemyAutoSchema):class Meta:
+        model = User
+        load_instance = True
