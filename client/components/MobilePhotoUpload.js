0c110147faba43b1f867fe7c5fd176ed6b232173
Joseph <jcschenk8@gmail.com>
2021-03-15 16:54:19 +0000

    mobile friendly upload complete

---
+++ client/components/MobilePhotoUpload.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useHistory, Link } from 'react-router-dom'
+
+
+export default function MobilePhotoUpload() {
+
+  const [photoData, updatePhotoData] = useState({
+    ...photoData,
+    url: '',
+    caption: ''
+  })
+  const [uploadSuccess, updateUploadSuccess] = useState(false)
+  const [captionError, updateCaptionError] = useState('')
+  const token = localStorage.getItem('token')
+  const history = useHistory()
+  const [photoError, updatePhotoError] = useState('')
+  function handleUpload(event) {
+    event.preventDefault()
+    window.cloudinary.createUploadWidget(
+      {
+        cloudName: 'dqkixqgcu',
+        uploadPreset: 'nasx6xsf',
+        cropping: true
+      },
+      (err, result) => {
+        if (result.event !== 'success') {
+          return
+        }
+        updatePhotoData({
+          ...photoData,
+          url: result.info.secure_url
+        })
+        updateUploadSuccess(true)
+        updatePhotoError('')
+      }
+    ).open()
+  }
+  function handleChange(event) {
+    const { name, value } = event.target
+
+    updatePhotoData({ ...photoData, [name]: value })
+    updateCaptionError('')
+  }
+
+  async function handleSave(event) {
+    event.preventDefault()
+    if (!photoData.caption) {
+      updateCaptionError('Please add a caption to your photo')
+      return
+    } else if (!photoData.url) {
+      updatePhotoError('Please upload a photo')
+      return
+    } else {
+      try {
+        await axios.post('/api/photos', photoData, {
+          headers: { Authorization: `Bearer ${token}` } 
+        })
+        history.push('/explore')
+      } catch (err) {
+        console.log(err)
+        console.log(err.response)
+      }
+    }
+  }
+
+  return <div>
+    <div className='container has-text-centered'>
+      <p className='title'>Hello!</p>
+      <p className='subtitle'>We've noticed you're on mobile, if you'd like to edit your photo before you upload it, please check out our website on desktop!</p>
+      <p className='subtitle'>Otherwise, please upload and caption your photo below</p>
+      <form className='field' onSubmit={handleSave}>
+        <div className='field'>
+          <div className='control'>
+            <button className="button is-hovered is-rounded" onClick={handleUpload}>Upload a picture</button>
+            {uploadSuccess && <div><small className="has-text-primary">Upload Complete</small></div>}
+            {photoError && <div><small className='has-text-primary'>{photoError}</small></div>}
+          </div>
+        </div>
+        <div className='field'>
+          <div className='control'>
+            <input className='input'
+              type='text'
+              value={photoData.caption}
+              onChange={handleChange}
+              name={'caption'}
+              placeholder='Please add a photo caption'
+            />
+            {captionError && <small className='has-text-primary'>{captionError}</small>}
+          </div>
+        </div>
+        {token ? <button className='button is-hovered is-rounded' onClick={handleSave}>Save and Publish</button>
+          : <p className='subtitle'>Please <Link to='/login'><em>login</em></Link> to save a photo!</p>}
+      </form>
+    </div>
+  </div>
+}
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -10,7 +10,24 @@ const Navbar = ({ history }) => {
   const [mobNav, updateMobNav] = useState(false)
   const [userId, setUserId] = useState('')
   const [userName, setUserName] = useState('')
-
+  const [isMobile, updateIsMobile] = useState(false)
+  
+  useEffect(() => {
+    function determineMobile(x) {
+      if (x.matches) { // If media query matches
+        updateIsMobile(true)
+      } else {
+        updateIsMobile(false)
+      }
+    }
+    
+    var x = window.matchMedia('(max-width: 500px)')
+    determineMobile(x) // Call listener function at run time
+    x.addListener(determineMobile) // Attach listener function on state changes
+  }, [])
+  
+  
+  
   useEffect(() => {
     const handleLogin = () => {
       const token = localStorage.getItem('token')
@@ -41,38 +58,38 @@ const Navbar = ({ history }) => {
   }
 
   return <nav className="navbar" role="navigation" aria-label="main navigation">
-  <div className="navbar-brand" id="navbarImage">
-  <Link to="/"><img src={logo} width="100" /></Link>
-    <a onClick={() => updateMobNav(!mobNav)} role="button" className={`navbar-burger ${mobNav ? 'is-active' : ''}`} >
-      <span aria-hidden="true" />
-      <span aria-hidden="true" />
-      <span aria-hidden="true" />
-    </a>
-  </div>
-  <div id="navbarBasicExample" className={`navbar-menu ${mobNav ? 'is-active' : ''}`}>
-    <div className="navbar-start">
+    <div className="navbar-brand" id="navbarImage">
+      <Link to="/"><img src={logo} width="100" /></Link>
+      <a onClick={() => updateMobNav(!mobNav)} role="button" className={`navbar-burger ${mobNav ? 'is-active' : ''}`} >
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
+    </div>
+    <div id="navbarBasicExample" className={`navbar-menu ${mobNav ? 'is-active' : ''}`}>
+      <div className="navbar-start">
 
 
-      <div className="navbar-item has-dropdown is-hoverable">
+        <div className="navbar-item has-dropdown is-hoverable">
 
-        <div className="navbar-dropdown">
-          <hr className="navbar-divider" />
+          <div className="navbar-dropdown">
+            <hr className="navbar-divider" />
+          </div>
+        </div>
+      </div>
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="menu-item">
+            <Link to="/explore" className="button is-light is-rounded">Explore</Link>
+            <Link className="button is-light is-rounded" to={isMobile ? "/mobile-photo-upload" : "/photo-upload"}>New Photo</Link>
+            {logIn && <Link to={`/profile/myprofile/${userName}`} className="button is-dark is-rounded">My Profile</Link>}
+            {!logIn && <Link to="/login" className="button is-dark is-rounded">Login</Link>}
+            {logIn && <button className="button is-light is-rounded" onClick={logOut}>Logout</button>}
+          </div>
         </div>
       </div>
     </div>
-    <div className="navbar-end">
-      <div className="navbar-item">
-        <div className="menu-item">
-          <Link to="/explore" className="button is-light is-rounded">Explore</Link>
-          <Link className="button is-light is-rounded" to="photo-upload">New Photo</Link>
-          {logIn && <Link to={`/profile/myprofile/${userName}`} className="button is-dark is-rounded">My Profile</Link>}
-          {!logIn && <Link to="/login" className="button is-dark is-rounded">Login</Link>}
-          {logIn && <button className="button is-light is-rounded" onClick={logOut}>Logout</button>}
-        </div>
-      </div>
-    </div>
-  </div>
-</nav >
+  </nav >
 
 
 }
