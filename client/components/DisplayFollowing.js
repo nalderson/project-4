4191235a9069ea7a7d6ad3ada9e56295865d56b1
Emily Randall <ej.randall@me.com>
2021-03-16 17:15:44 +0000

    follow buttons + disabling

--- client/components/DisplayFollowing.js
+++ client/components/DisplayFollowing.js
@@ -27,8 +27,8 @@ export default function DisplayFollowing(props) {
 
   return <>
     <div className="buttons has-addons is-center">
-      <button id="followers-button"className="account-buttons button is-dark is-rounded" onClick={() => showModal2(!modal2)}>Followers</button>
-      <button id="following-button" className="account-buttons button is-dark is-rounded" onClick={() => showModal(!modal)}>Following</button>
+      <button id="followers-button"className="account-buttons button is-dark is-rounded" onClick={() => showModal2(!modal2)} disabled={!profile.following_users}>Followers</button>
+      <button id="following-button" className="account-buttons button is-dark is-rounded" onClick={() => showModal(!modal)} disabled={!profile.following_current_user}>Following</button>
     </div>
     <div role="button" className={`modal ${modal ? 'is-active' : ''}`}>
       <div className="modal is-active">
@@ -51,9 +51,12 @@ export default function DisplayFollowing(props) {
                         </p>
                       </figure>
                       <div className="media-content">
-                        <div className="content is-flex">
+                        <div className="content">
                           <h1>{console.log(follower.following_user.username)}</h1>
-                          <p><strong>{follower.following_user.username}</strong></p>
+                          <p>
+                            <strong>{follower.following_user.username}</strong>
+                            <br>Please show the name that'd be great thanks x</br>
+                            </p>
                         </div>
                       </div>
                     </Link>
@@ -73,29 +76,26 @@ export default function DisplayFollowing(props) {
         <div className="modal-background"></div>
         <div className="modal-card">
           <header className="modal-card-head">
-            <p className="modal-card-title">Following</p>
+            <p className="modal-card-title">Followers</p>
             <button className="delete" aria-label="close" onClick={() => showModal2(!modal2)} />
           </header>
           <section className="modal-card-body">
             <div className="container is-centered">
-              {profile.following_current_user.map((follower, index) => {
-                console.log(profile.following_current_user)
-                if (follower.following_user) {
+               {(profile.following_users) && profile.following_users.map((follower, index) => {
                   return <article key={index} className="media">
-                    <Link to={{ pathname: `/profile/${follower.following_user.id}` }}>
+                    <Link to={{ pathname: `/profile/${follower.following_current_user.id}` }}>
                       <figure className="media-left">
                         <p className="image is-64x64">
-                          <img src={follower.following_user.profile_picture} alt={follower.following_user.username} />
+                          <img src={follower.following_current_user.profile_picture} alt={follower.following_current_user.username} />
                         </p>
                       </figure>
                       <div className="media-content">
                         <div className="content is-flex">
-                          <p><strong>{follower.following_user.username}</strong></p>
+                          <p><strong>{follower.following_current_user.username}</strong></p>
                         </div>
                       </div>
                     </Link>
                   </article>
-                }
               })}
             </div>
           </section>
--- client/components/FollowerProfile.js
+++ client/components/FollowerProfile.js
@@ -1,43 +1,63 @@
-import React, { useState, useEffect, Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import DisplayFollowing from './DisplayFollowing'
-
+import { getLoggedInUserId } from '../lib/auth'
 
 export default function Explore({ match }) {
 
+  const [following, updateFollowing] = useState(false)
   const [profile, updateProfile] = useState({
     photos: [],
   })
-
+  const token = localStorage.getItem('token')
   const user_id = match.params.user_id
-  console.log(match.params.username)
 
   useEffect(() => {
     async function getProfileData() {
       try {
         const { data } = await axios.get(`/api/profile/${user_id}`)
+        console.log(data)
+        console.log(data.following_current_user)
+        data.following_current_user.map((user) => {
+        
+            if (user.following_user.id === getLoggedInUserId()) {
+              updateFollowing(true)
+            }
+          
+        })
         updateProfile(data)
       } catch (err) {
         console.log(err)
       }
     }
     getProfileData()
-  }, [])
+  }, [following])
+
+  async function followUser() {
+    try {
+      if (!following) {
+      await axios.post(`/api/profile/${user_id}`, {}, { headers: { 'Authorization': `Bearer ${token}` } })
+          console.log(following, 'line 34')
+          updateFollowing(true)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return <div className="container is-vcentered">
     <div className="container is-vcentered block box" id="account-header">
       <img id="account-profile-pic" src={profile.profile_picture} />
       <h1 id="account-title" className="title">{profile.username}</h1>
-      <div className="container is-center">
-        {/* <DisplayFollowing /> */}
+      <div className="container">
+        {!following && <div className="button is-light is-rounded" onClick={followUser}>Follow</div>}
+        {following && <div className="button is-light is-rounded" disabled>Following</div>}
       </div>
     </div>
     <div>
       <section className="section is-centered">
         <div className="container is-centered">
           <div className="columns is-multiline is-mobile is-centered">
-            <h2>User Photos</h2>
             {profile.photos.map((photo, index) => {
               return <div key={index} className="column is-one-third-desktop is-half-tablet is-half-mobile is-centered">
                 <Link to={`/explore/${photo.id}`}>
